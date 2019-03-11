521b38c066d9950c954f1eb4bbd8f270e936ae3d
Nicknamee <nicktse2001@hotmail.com>
2019-03-11 14:50:56 +0100

    Initial commit

---
+++ opdracht2.1/js1.js
@@ -0,0 +1,4 @@
+document.getElementById("titel").innerText = "Dit is de pagina titel";
+document.getElementById("subkop").innerText = "Dit is de subkop";
+document.getElementById("pic1").src = "img/banaan1.jpg";
+document.getElementById("pic2").src = "img/banaan2.jpg";
