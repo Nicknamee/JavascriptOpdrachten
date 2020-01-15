8ed585da8c2c8e59d0e720421a9d0674a13f7428
Nicknamee <nicktse2001@hotmail.com>
2020-01-15 10:47:41 +0100

    Javascript opdrachten

---
+++ opdracht2.2/js1.js
@@ -0,0 +1,29 @@
+
+var a = document.getElementById('bl1');
+a.style.position = "absolute";
+a.style.left = '0px';
+a.style.top = '0px';
+a.style.backgroundColor = "#28FFFF";
+a.style.textAlign = "center";
+a.style.paddingTop= "20px";
+
+var b = document.getElementById('bl2');
+b.style.position = "absolute";
+b.style.left = '100px';
+b.style.top = '20px';
+b.style.backgroundColor = "#0B8012";
+b.style.textAlign = "center";
+
+var c = document.getElementById('bl3');
+c.style.position = "absolute";
+c.style.left = '200px';
+c.style.top = '50px';
+c.style.backgroundColor = "#FF000";
+c.style.textAlign = "right";
+
+var d = document.getElementById('bl4');
+d.style.position = "absolute";
+d.style.left = '300px';
+d.style.top = '0px';
+d.style.backgroundColor = "#FF0000";
+d.style.textAlign = "left";
